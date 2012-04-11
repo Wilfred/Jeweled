49a46cb6e6a76e18e37a2d16270a628a51a1b7af
Wilfred Hughes <me@wilfred.me.uk>
2012-04-12 00:02:52 +0100

    initial commit, just some notes

---
+++ bejeweled.py
@@ -0,0 +1,16 @@
+"""Bejeweled: An automated bot for HTML 5 Bejeweled.
+
+Bejeweled is at: http://bejeweled.popcap.com/html5/
+Mode targeted: Classic
+
+Scoring in first round:
+
+3 in a row: 50
+Each subsequent row scores 50 more: 50, 100, 150, and so on.
+
+4 in a row: 100
+Also gains a 'fire gem'.
+
+Scores in subsequent rounds increase by 50 points per level.
+
+"""
