2f495eb83747df7549a916522f254010fc6cdfd9
Wilfred Hughes <me@wilfred.me.uk>
2012-04-12 00:16:56 +0100

    constants based on measuring the board and grid

--- bejeweled.py
+++ bejeweled.py
@@ -18,6 +18,13 @@ Scores in subsequent rounds increase by 50 points per level.
 import autopy
 from time import sleep
 
+# TODO: dynamically calculate board position
+BOARD_X = 1092
+BOARD_Y = 253
+
+GRID_WIDTH = GRID_HEIGHT = 80
+GRID_SIZE = 8
+
 if __name__ == '__main__':
     # TODO: use xdg-open
     print "Starting in 10 seconds, please switch to bejeweled"
