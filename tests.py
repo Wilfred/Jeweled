b82e07b48136bf116462e81149f22552370b1115
Wilfred Hughes <me@wilfred.me.uk>
2012-09-22 10:10:33 +0100

    assert the board position in our basic board detection test

--- tests.py
+++ tests.py
@@ -7,7 +7,11 @@ from board import get_position
 class BoardDetectionTest(TestCase):
     def test_basic_detection(self):
         bitmap = autopy.bitmap.Bitmap.open("sample_images/board1.png")
-        get_position(bitmap) # throws board.NotVisible if no position
+        x, y = get_position(bitmap) # throws board.NotVisible if no position
+
+        # since the board positioning is imprecise, we just assert a range
+        assert 260 < x < 270
+        assert 100 < y < 110
 
     def test_darker_board_detection(self):
         bitmap = autopy.bitmap.Bitmap.open("sample_images/board_dark_background.png")
