18b64a7c9ea324e062ef85d8f367ba1383a50e8a
Wilfred Hughes <me@wilfred.me.uk>
2012-09-22 10:08:35 +0100

    adding a (failing) unit test for a board with a dark background

--- tests.py
+++ tests.py
@@ -9,3 +9,7 @@ class BoardDetectionTest(TestCase):
         bitmap = autopy.bitmap.Bitmap.open("sample_images/board1.png")
         get_position(bitmap) # throws board.NotVisible if no position
 
+    def test_darker_board_detection(self):
+        bitmap = autopy.bitmap.Bitmap.open("sample_images/board_dark_background.png")
+        get_position(bitmap) # throws board.NotVisible if no position
+
