b45bbb1c8d52a120a0657075af3b4cae079c2b3b
Wilfred Hughes <me@wilfred.me.uk>
2012-09-22 10:29:46 +0100

    using the edge of the game screen for board detection (fixes unit test)

--- src/board.py
+++ src/board.py
@@ -11,16 +11,17 @@ SIZE = 8 # number of row elements/column elements in grid
 
 def get_position(screen_bitmap):
     """Find the co-ordinates of the top left corner of the board."""
-    board_corner = autopy.bitmap.Bitmap.open('resources/board_corner.png')
+    board_corner = autopy.bitmap.Bitmap.open('resources/board_edge.png')
 
+    # we search for the left edge of the board. Since autopy searches
+    # left to right, find_bitmap returns the correct co-ordinates
     board_position = screen_bitmap.find_bitmap(board_corner)
 
     if board_position:
         raw_x, raw_y = board_position
 
-        # the board position is not on the board, so the actual
-        # top-left corner is slightly offset:
-        return (raw_x + 1, raw_y + 9)
+        # the board itself is offset from the left edge
+        return (raw_x + 199, raw_y + 32)
     else:
         # TODO: also check bottom right corner is visible
         raise NotVisible()
